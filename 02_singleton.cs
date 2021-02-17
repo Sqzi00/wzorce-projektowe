bfe4c595f9e0e83dbe96d36ec4cdb226b66ee75c
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-17 10:48:34 +0100

    Add files via upload
    Singelton

---
+++ 02_singleton.cs
@@ -0,0 +1,61 @@
+// IMIE NAZWISKO - NR ALBUMU - GRUPA LAB DO KTOREJ NALEZE - GRUPA LAB NA KTOREJ JESTEM NA ZAJECIACH
+// (przyklad) Iga Swiatek - 101020 - lab2/FED - lab4/FED
+
+using System;
+using System.Collections.Generic;
+
+namespace labs
+{
+    class Lab2Singleton: Lab
+    {
+        public void Run() {
+            getRepositories();
+            getStars();   
+        }
+
+        private void getRepositories() {
+            Console.WriteLine("\nGET REPOSITORIES");
+            GithubRestApi restApi = new GithubRestApi();
+            restApi.getBestUsers();
+        }
+
+        private void getStars() {
+            Console.WriteLine("\nGET STARS");
+            GithubRestApi restApi = new GithubRestApi();
+            restApi.getBestUsers();
+        }
+    }
+
+    class GithubRestApi
+    {
+        private String host;
+        private List<int> bestUserIds;
+        List<int> numbers = new List<int>();
+
+
+        public GithubRestApi()
+        {
+            
+            this.host = "https://api.github.com";
+            this.bestUserIds = new List<int>();
+
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                this.bestUserIds.Add(random.Next());
+            }
+        }
+
+        public void getBestUsers()
+        {
+            foreach (int userId in bestUserIds) {
+                this.makeCall("/user/"+userId);
+            }
+        }
+
+        private void makeCall(String path)
+        {
+            Console.WriteLine(host + path);
+        }
+    }
+}
